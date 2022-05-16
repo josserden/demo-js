66f354056134d26d57ad4b1de6629ac3304b1508
Den <52295780+josserden@users.noreply.github.com>
2022-05-16 19:26:43 +0300

    create lesson

---
+++ js/task5.js
@@ -0,0 +1,7 @@
+// todo Масиви та рядки
+/*
+ * Напиши скрипт який виводить у консоль усі слова рядка крім першого та останнього. Результуючий рядок не повинен починатися або закінчуватися пробілом. Скрипт повинен працювати для будь-якого рядка.
+ */
+
+const string =
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas cupiditate excepturi,';
